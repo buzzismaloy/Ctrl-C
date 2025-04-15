5b99112c822461ef779d3eb6dc005b3f451a7eb2
nyxnyx <nyxnyx@archenemy>
2025-04-15 22:10:33 +0300

    Implemented strings highlighting

--- ctrlc.c
+++ ctrlc.c
@@ -22,7 +22,9 @@
 #define CTRLC_VERSION "1.0"
 #define CTRLC_TAB_STOP 8
 #define CTRLC_QUIT_TIMES 2
+
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 /* data */
 typedef struct erow {
@@ -65,6 +67,7 @@ enum editorKey {
 
 enum editorHighlight {
 	HL_NORMAL = 0,
+	HL_STRING,
 	HL_NUMBER,
 	HL_MATCH
 };
@@ -84,7 +87,7 @@ struct editorSyntax HLDB[] = {
 	{
 		"C",
 		C_HL_extensions,
-		HL_HIGHLIGHT_NUMBERS
+		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
 	},
 };
 
@@ -368,11 +371,37 @@ void editorUpdateSyntax(erow* row) {
 	if (E.syntax == NULL) return;
 
 	int prev_sep = 1;
+	int in_string = 0;
 
 	int i = 0;
 	while (i < row->render_size) {
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+		if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+			if (in_string) {
+				row->hl[i] = HL_STRING;
+
+				if (c == '\\' && i + 1 < row->render_size) {
+					row->hl[i + 1] = HL_STRING;
+					i += 2;
+					continue;
+				}
+
+				if (c == in_string) in_string = 0;
+				++i;
+				prev_sep = 1;
+				continue;
+			}
+			else {
+				if (c == '"' || c == '\'') {
+					in_string = c;
+					row->hl[i] = HL_STRING;
+					++i;
+					continue;
+				}
+			}
+		}
 
 		if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
 			if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
@@ -391,6 +420,7 @@ void editorUpdateSyntax(erow* row) {
 
 int editorSyntaxToColor(int hl) {
 	switch (hl) {
+		case HL_STRING: return 35;
 		case HL_NUMBER: return 31;
 		case HL_MATCH: return 34;
 
